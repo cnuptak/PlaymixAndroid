6e693a65b455fc01e6f6dc3851e826cab6ad8d16
cnuptak <cfkzvbcfkzvb@gmail.com>
2020-05-19 23:23:31 +0300

    added ShareButton in Tracklist

--- app/src/main/java/com/example/playmixandroid/TracklistFragment.kt
+++ app/src/main/java/com/example/playmixandroid/TracklistFragment.kt
@@ -1,10 +1,9 @@
 package com.example.playmixandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 
 class TracklistFragment : Fragment() {
@@ -13,6 +12,30 @@ class TracklistFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_tracklist, container, false)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.sharetracklistmenu, menu)
+    }
+
+    private fun getShareIntent() : Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT,getString(R.string.tracklist))
+        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId) {
+            R.id.sharemenu -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
