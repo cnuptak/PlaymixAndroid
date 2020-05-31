f2d80fddca5d8b23b9454446dbbdb292b6034880
cnuptak <cfkzvbcfkzvb@gmail.com>
2020-05-31 21:01:33 +0300

    added basic seekbar

--- app/src/main/java/com/example/playmixandroid/MainActivity.kt
+++ app/src/main/java/com/example/playmixandroid/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.playmixandroid
 
-import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,16 +14,15 @@ import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.playmixandroid.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.fragment_title.*
 import timber.log.Timber
-import android.widget.Button
-import android.widget.Toast
-import android.widget.SeekBar
-
 
 class MainActivity : AppCompatActivity() {
     private var mediaPlayer : MediaPlayer? = null
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var trackTimer: TrackTimer
+    private lateinit var runnable: Runnable
+    private var handler: Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         mediaPlayer = MediaPlayer.create(this, R.raw.april)
+
         mediaPlayer?.setOnPreparedListener {
             println("Ready to go")
             val playButton: Button = findViewById(R.id.play_button)
@@ -50,22 +53,59 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show()
                 } else {
                     mediaPlayer?.start()
+                    initializeSeekBar()
                     playButton.setBackgroundResource(R.drawable.ic_pause_circle_outline_black_24dp)
                     Toast.makeText(this, "Playing the file", Toast.LENGTH_SHORT).show()
                 }
-
+            }
+            val stopButton: Button = findViewById(R.id.stop_button)
+            stopButton.setOnClickListener{
+                seek_bar.setProgress(0)
+                mediaPlayer?.stop()
+                Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show()
             }
         }
-//        mediaPlayer?.setOnPreparedListener {
-//            println("Ready to go")
-//            val playButton: Button = findViewById(R.id.play_button)
-//            playButton.setOnClickListener {
-//                mediaPlayer?.start()
-//                Toast.makeText(this, "Playing the file", Toast.LENGTH_SHORT).show()
-//            }
-//        }
+
         trackTimer = TrackTimer(this.lifecycle)
+
+        seek_bar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mediaPlayer?.seekTo(progress * 1000)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
     }
+
+    private fun initializeSeekBar() {
+        seek_bar.max = mediaPlayer?.seconds!!
+        runnable = Runnable{
+            seek_bar.progress = mediaPlayer?.currentSeconds!!
+            current_time.text = "${mediaPlayer?.currentSeconds} sec"
+
+            val timepos = mediaPlayer?.seconds!!
+            duration.text = "$timepos sec"
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    val MediaPlayer.seconds:Int
+        get() {
+            return this.duration / 1000
+        }
+
+    val MediaPlayer.currentSeconds:Int
+        get() {
+            return this.currentPosition/1000
+        }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
